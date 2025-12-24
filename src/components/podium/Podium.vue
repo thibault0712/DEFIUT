854cb662e229fda3debcf487633d8e4c882b5fca
Falézan <thibaultfalezan@gmail.com>
2025-12-24 17:17:43 +0100

    Feature : Create Home page

---
+++ src/components/podium/Podium.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container class="d-flex align-end justify-center py-10" style="min-height: 500px;">
+
+    <div class="d-flex flex-column align-center mx-1">
+      <v-avatar color="secondary" size="80" class="mb-4 elevation-2">
+        <span class="text-h6">VJ</span>
+      </v-avatar>
+      <v-sheet
+        color="primary"
+        width="140"
+        height="180"
+        class="rounded-t-lg d-flex align-center justify-center"
+      >
+        <v-avatar color="brown-darken-3" size="60" class="title-orbitron">
+          <span class="text-h4">3</span>
+        </v-avatar>
+      </v-sheet>
+    </div>
+
+    <div class="d-flex flex-column align-center mx-1">
+      <v-avatar color="secondary" size="100" class="mb-4 elevation-4 border-lg">
+        <span class="text-h5">VJ</span>
+      </v-avatar>
+      <v-sheet
+        color="primary"
+        width="160"
+        height="280"
+        class="rounded-t-lg d-flex align-center justify-center shadow-lg"
+      >
+        <v-avatar color="orange-darken-1" size="80" class="title-orbitron elevation-6">
+          <span class="text-h3 font-weight-bold">1</span>
+        </v-avatar>
+      </v-sheet>
+    </div>
+
+    <div class="d-flex flex-column align-center mx-1">
+      <v-avatar color="secondary" size="80" class="mb-4 elevation-2">
+        <span class="text-h6">VJ</span>
+      </v-avatar>
+      <v-sheet
+        color="primary"
+        width="140"
+        height="220"
+        class="rounded-t-lg d-flex align-center justify-center"
+      >
+        <v-avatar color="grey-darken-3" size="70" class="title-orbitron">
+          <span class="text-h4">2</span>
+        </v-avatar>
+      </v-sheet>
+    </div>
+
+  </v-container>
+</template>
